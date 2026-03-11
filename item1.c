ade7b5b01a8ad9c6e2fd201600392bc66da51d42
anakilciauskas <anakilciauskas@gmail.com>
2026-03-11 15:32:07 -0300

    Add files via upload

---
+++ item1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int valor = 100; //declara uma variavel do tipo inteiro e atribui 100 a ela
+	printf("%d", valor); //exibe na tela o valor da variavel
+
+	return 0;
+}
