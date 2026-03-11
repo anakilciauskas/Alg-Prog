861afb96f72b4e6f78401b26a73233360eea65d9
anakilciauskas <anakilciauskas@gmail.com>
2026-03-11 15:42:10 -0300

    Update and rename item3.c to L0I3.c

---
+++ L0I3.c
@@ -0,0 +1,12 @@
+/*Declare uma variável do tipo char.
+Atribua a letra 'B' e mostre no console.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char letra = 'B';
+	printf("%c", letra);
+	
+	return 0;
+}
--- item3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-	char letra = 'B'; //declara uma variavel do tipo caractere e atribui B a ela
-	printf("%c", letra); //exibe na tela o valor (letra) da variavel
-	
-	return 0;
-}
