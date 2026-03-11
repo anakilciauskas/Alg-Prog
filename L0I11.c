f9a395a44679a57040cabcfff6d66bb94c5795f8
anakilciauskas <anakilciauskas@gmail.com>
2026-03-11 15:55:06 -0300

    Update and rename item11.c to L0I11.c

---
+++ L0I11.c
@@ -0,0 +1,12 @@
+/*Declare uma variável que será usada para armazenar o gênero de uma
+pessoa, podendo receber apenas 'M' ou 'F'. Atribua um dos valores e mostre no console.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+	int main(){
+		char genero = 'M';
+		printf("O genero e %c", genero);
+	
+		return 0;
+	}
