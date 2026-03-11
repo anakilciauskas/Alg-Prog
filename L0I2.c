5b1f29538f816b89b944177d206e96d4e03198c3
anakilciauskas <anakilciauskas@gmail.com>
2026-03-11 15:40:48 -0300

    Update and rename item2.c to L0I2.c

---
+++ L0I2.c
@@ -0,0 +1,12 @@
+/*Declare uma variável do tipo float. Atribua o valor 7.5
+e exiba no console com 2 casas decimais*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	float valor = 7.5;
+	printf("%.2f", valor);
+	
+	return 0;
+}
