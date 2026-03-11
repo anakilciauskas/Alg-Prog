1ee5ef51f497082902b9973fbcad7c1361ceb5d2
anakilciauskas <anakilciauskas@gmail.com>
2026-03-11 15:46:05 -0300

    Update and rename item6.c to L0I6.c

---
+++ L0I6.c
@@ -0,0 +1,13 @@
+/*Declare uma variável que será usada para armazenar a idade de uma pessoa.
+Atribua um valor adequado e mostre no console.*/
+
+#include <stdio.h>
+
+	int main(){
+		int idade;
+		printf("qual sua idade?\n");
+		scanf("%d", &idade);
+		printf("A idade e %d", idade);
+		
+		return 0;
+	}
