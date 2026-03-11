e3917ac0cdf505b6f8a1b9afa47d30ece742288e
anakilciauskas <anakilciauskas@gmail.com>
2026-03-11 15:51:13 -0300

    Update and rename item8.c to L0I8.c

---
+++ L0I8.c
@@ -0,0 +1,13 @@
+/*Declare uma variável que será usada para armazenar a letra inicial do nome
+de uma pessoa. Atribua um valor e mostre no console.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+	int main(){
+		char inicial = 'A';
+		printf("A inicial do nome e %c", inicial);
+	
+		return 0;
+
+	}
