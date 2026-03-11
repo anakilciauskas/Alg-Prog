95e5864cb8f6d5500937646a7fed57694101ddca
anakilciauskas <anakilciauskas@gmail.com>
2026-03-11 15:50:13 -0300

    Update and rename item7.c to L0I7.c

---
+++ L0I7.c
@@ -0,0 +1,11 @@
+/*Declare uma variável que será usada para armazenar a altura de uma pessoa
+em metros. Atribua um valor e mostre com 2 casas decimais.*/
+
+#include <stdio.h>
+
+	int main(){
+		float alt = 1.7;
+		printf("sua altura em metros e %.2f", alt);
+		
+		return 0;
+	}
