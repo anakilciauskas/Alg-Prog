e7f277002f32ad6915c21cc2f2678399555b29b6
anakilciauskas <anakilciauskas@gmail.com>
2026-03-11 15:32:59 -0300

    Add files via upload

---
+++ item1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+	int main(){
+		float num;
+		float num2;
+		printf("Insira um numero\n");
+		scanf("%f", &num);
+		num2 = num*2;
+		printf("O dobro desse numero e %.2f", num2);
+		
+	
+		return 0;
+	}
