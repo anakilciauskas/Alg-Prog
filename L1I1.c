9431f14f2c4f6d2a1153827db4155a23183c3d54
anakilciauskas <anakilciauskas@gmail.com>
2026-03-11 16:01:08 -0300

    Update and rename item1.c to L1I1.c

--- L1I1.c
+++ L1I1.c
@@ -1,14 +1,15 @@
+/*Receber um número, calcular seu dobro e exibir o resultado*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 	int main(){
-		float num;
+		float num1;
 		float num2;
 		printf("Insira um numero\n");
-		scanf("%f", &num);
+		scanf("%f", &num1);
 		num2 = num*2;
 		printf("O dobro desse numero e %.2f", num2);
-		
 	
 		return 0;
 	}
