594b840ec3aabac1005ca7f255f0daaff2398f28
anakilciauskas <anakilciauskas@gmail.com>
2026-03-11 16:07:28 -0300

    Update and rename item7.c to L1I7.c

--- L1I7.c
+++ L1I7.c
@@ -1,3 +1,7 @@
+/*Receber uma temperatura em Fahrenheit e convertê-la para Celsius
+através da seguinte fórmula: Celsius = (Fahrenheit - 32) / 1.8. 
+Exibir o valor em Celsius*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
