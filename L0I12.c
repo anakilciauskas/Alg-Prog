447f83e6ac4113a9948d44fbb24940a766d379fe
anakilciauskas <anakilciauskas@gmail.com>
2026-03-11 15:56:04 -0300

    Update and rename item12.c to L0I12.c

---
+++ L0I12.c
@@ -0,0 +1,12 @@
+/*Declare uma variável que será usada para armazenar um número com muitas casas decimais (exemplo: 3.1415926535). 
+Atribua um valor adequado e mostre no console com 8 casas decimais*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+	int main(){
+		float pi = 3.1415926535;
+		printf("O valor de pi e %.8f", pi);
+	
+		return 0;
+	}
--- item12.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-	int main(){
-		float pi = 3.1415926535; //declara uma variavel do tipo float e atribui o valor a ela
-		printf("O valor de pi e %.8f", pi); //exibe na tela a frase e o valor da variavel com oito casas decimais
-	
-		return 0;
-	}
