c23bd5dc79d1f3b8fbd5fb67cc647968b9a885a9
anakilciauskas <anakilciauskas@gmail.com>
2026-03-11 15:45:12 -0300

    Update and rename item5.c to L0I5.c

---
+++ L0I5.c
@@ -0,0 +1,11 @@
+/*Declare uma variável do tipo double.
+Atribua o valor 3.141592 e mostre o valor com 4 casas decimais*/
+
+#include <stdio.h>
+
+	int main(){
+		double pi = 3.141592;
+		printf("O valor e: %.4lf", pi);
+		
+		return 0;
+	}
