fb5a2f21d3c6ea0e9bdd9fdd894fcd932ce7c910
anakilciauskas <anakilciauskas@gmail.com>
2026-03-11 15:52:11 -0300

    Update and rename item9.c to L0I9.c

---
+++ L0I9.c
@@ -0,0 +1,12 @@
+/*Declare uma variável que será usada para armazenar o preço de um produto
+com centavos. Atribua um valor e mostre com 2 casas decimais.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+	int main(){
+		float preco = 34.9912;
+		printf("O preco e %.2f", preco);
+	
+		return 0;
+	}
--- item9.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-	int main(){
-		float preco = 34.9912; //declara uma variavel do tipo float e atribui a ela 34.9912;
-		printf("O preco e %.2f", preco); //exibe na tela a frase o valor da variavel com duas casas decimais
-	
-		return 0;
-	}
