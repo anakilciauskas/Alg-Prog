f2da58d91caaca3468e8aae12a87a892dafe9ea5
anakilciauskas <anakilciauskas@gmail.com>
2026-03-11 15:54:17 -0300

    Update and rename item10.c to L0I10.c

---
+++ L0I10.c
@@ -0,0 +1,12 @@
+/*Declare uma variável que será usada para armazenar a quantidade de alunos
+em uma sala. Atribua um valor e exiba no console*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+	int main(){
+		int alunos = 64;
+		printf("A quantidade de alunos e %d", alunos);
+	
+		return 0;
+	}
