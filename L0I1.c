1b3c7ba123e9bfefd5df80252e59b2ab7d38cec3
anakilciauskas <anakilciauskas@gmail.com>
2026-03-11 15:36:25 -0300

    Update and rename item1.c to L0I1.c

---
+++ L0I1.c
@@ -0,0 +1,13 @@
+/*Declare uma variável do tipo inteiro, atribua a ela o valor 100 e
+mostre o conteúdo da variável no console.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int valor = 100;
+	printf("%d", valor);
+
+	return 0;
+}
+
